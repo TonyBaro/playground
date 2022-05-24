f38080ac8ec3e1e95315660e1f26af92a9273851
TonyB <tonybaromagic@gmail.com>
2022-05-24 19:24:29 -0400

    level 1 complete

---
+++ server.py
@@ -0,0 +1,11 @@
+from ast import Return
+from flask import Flask, render_template 
+app = Flask(__name__)  
+
+@app.route('/play')
+def play():
+    return render_template('index.html')
+
+
+if __name__=="__main__":  
+    app.run(debug=True) 
